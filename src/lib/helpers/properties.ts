862e729133118342356953a4ce8bb266e12a4dc0
cheap-glitch <cheap.glitch@gmail.com>
2021-01-04 15:03:05 +0100

    🚩 test: Add tests for `formatPropertiesPath()`

--- src/lib/helpers/properties.ts
+++ src/lib/helpers/properties.ts
@@ -1,9 +1,12 @@
 export type PropertiesPath = Array<string | number>;
 
 export function formatPropertiesPath(path: PropertiesPath): string {
-	return path.length > 0 ? ('.' + path.map(pathSegment => typeof pathSegment === 'number' ? ('[' + pathSegment + ']') : pathSegment).join('.')) : '';
+	return path.map(pathSegment => (typeof pathSegment === 'number') ? ('[' + pathSegment + ']') : ('.' + pathSegment)).join('');
 }
 
 export function parsePropertiesPath(rawPath: string): PropertiesPath {
-	return rawPath.split('.').filter(Boolean).map(pathSegment => /^\d+$/.test(pathSegment) ? Number.parseInt(pathSegment, 10) : pathSegment);
+	return rawPath
+		.replace(/\[(?<index>\d+)\]/g, '.$<index>')
+		.split('.').filter(Boolean)
+		.map(pathSegment => /^\d+$/.test(pathSegment) ? Number.parseInt(pathSegment, 10) : pathSegment);
 }
---
+++ test/helpers.test.ts
@@ -0,0 +1,49 @@
+import { parsePropertiesPath } from '../src/lib/helpers/properties';
+
+describe("parsePropertiesPath", () => {
+
+	test("empty path", () => { // {{{
+
+		expect(parsePropertiesPath('')).toEqual([]);
+
+	}); // }}}
+
+	test("single key", () => { // {{{
+
+		expect(parsePropertiesPath('foo' )).toEqual(['foo']);
+		expect(parsePropertiesPath('.foo')).toEqual(['foo']);
+
+	}); // }}}
+
+	test("mutiple keys", () => { // {{{
+
+		expect(parsePropertiesPath('foo.bar.baz'  )).toEqual(['foo', 'bar', 'baz']);
+		expect(parsePropertiesPath('.foo.bar.baz' )).toEqual(['foo', 'bar', 'baz']);
+		expect(parsePropertiesPath('.foo..bar.baz')).toEqual(['foo', 'bar', 'baz']);
+
+	}); // }}}
+
+	test("single numeric keys", () => { // {{{
+
+		expect(parsePropertiesPath('0'  )).toEqual([0]);
+		expect(parsePropertiesPath('.0' )).toEqual([0]);
+		expect(parsePropertiesPath('[0]')).toEqual([0]);
+
+	}); // }}}
+
+	test("mutiple numeric keys", () => { // {{{
+
+		expect(parsePropertiesPath('0.1.2'    )).toEqual([0, 1, 2]);
+		expect(parsePropertiesPath('[0][1][2]')).toEqual([0, 1, 2]);
+		expect(parsePropertiesPath('[0].1.[2]')).toEqual([0, 1, 2]);
+
+	}); // }}}
+
+	test("mutiple mixed keys", () => { // {{{
+
+		expect(parsePropertiesPath('.foo[0].bar.2'      )).toEqual(['foo', 0, 'bar', 2]);
+		expect(parsePropertiesPath('[0].foo..[1].bar[2]')).toEqual([0, 'foo', 1, 'bar', 2]);
+
+	}); // }}}
+
+});
