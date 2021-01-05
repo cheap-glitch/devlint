b80f866d1da5054e8f02546cee2c03e5c2c27e0e
cheap-glitch <cheap.glitch@gmail.com>
2021-01-05 13:48:40 +0100

    🌱 feat!: Limit execution to a single working directory

--- src/cli.ts
+++ src/cli.ts
@@ -16,6 +16,8 @@ export async function cli(): Promise<void> {
 		rules: { type: 'string',  default: '*',   description: 'Set specific rules to consider'               },
 	}).argv;
 
+	const workingDirectory = (typeof options._[0] === 'string') ? options._[0] : '.';
+
 	let config: JsonValue;
 	try {
 		config = JSON.parse(await readFileContents([homedir(), '.devlintrc.json']));
@@ -31,65 +33,55 @@ export async function cli(): Promise<void> {
 	if (rules.size === 0) {
 		return;
 	}
-	console.info(rules);
 
-	for (const folder of (options._.length === 0 ? ['.'] : options._)) {
-		if (typeof folder !== 'string') {
-			continue;
-		}
+	const results = await lint(joinPathSegments([process.cwd(), workingDirectory]), rules);
+	const totals  = {
+		errors:   0,
+		warnings: 0,
+		skipped:  0,
+	};
 
-		const results = await lint(joinPathSegments([process.cwd(), folder]), rules);
-		const totals  = {
-			errors:   0,
-			warnings: 0,
-			skipped:  0,
-		};
-
-		for (const [[targetFilePath, targetPropertiesPathSegments], targetResults] of results) {
-			const reports = targetResults.map(([rule, result]) => {
-				if (result === true) {
-					return '';
-				}
-
-				switch (result.type) {
-					case RuleErrorType.UnknownRule:
-						totals.skipped++;
-						return skippedRuleReport(rule, 'unknown rule');
-
-					case RuleErrorType.InvalidData:
-						totals.skipped++;
-						return skippedRuleReport(rule, 'invalid data or rule does not apply to target');
-
-					case RuleErrorType.InvalidParameters:
-						totals.skipped++;
-						return skippedRuleReport(rule, `invalid parameters (cf. https://devlint.org/rules/${rule.name})`);
-
-					case RuleErrorType.Failed:
-						switch (rule.status) {
-							case RuleStatus.Error:   totals.errors++;   break;
-							case RuleStatus.Warning: totals.warnings++; break;
-						}
-						return ruleErrorReport(rule, result);
-				}
-
-				return undefined;
-			})
-			.filter(Boolean);
-
-			if (!options.quiet && reports.length > 0) {
-				const fullTargetPath = getAbsolutePath([process.cwd(), folder, targetFilePath])
-					+ (targetPropertiesPathSegments.length > 0 ? '#' : '')
-					+ targetPropertiesPathSegments;
-
-				console.log('\n' + formattedHeader(fullTargetPath) + '\n' + reports.join('\n'));
+	for (const [[targetFsPath, targetPropertiesPathSegments], targetResults] of results) {
+		const reports = targetResults.map(([rule, result]) => {
+			if (result === true) {
+				return '';
 			}
-		}
 
-		if (!options.quiet && Object.values(totals).some(Boolean)) {
-			console.log('\n' + totalsReport(totals.errors, totals.warnings, totals.skipped) + '\n');
+			switch (result.type) {
+				case RuleErrorType.UnknownRule:
+					totals.skipped++;
+					return skippedRuleReport(rule, 'unknown rule');
+
+				case RuleErrorType.InvalidData:
+					totals.skipped++;
+					return skippedRuleReport(rule, 'invalid data or rule does not apply to target');
+
+				case RuleErrorType.InvalidParameters:
+					totals.skipped++;
+					return skippedRuleReport(rule, `invalid parameters (cf. https://devlint.org/rules/${rule.name})`);
+
+				case RuleErrorType.Failed:
+					switch (rule.status) {
+						case RuleStatus.Error:   totals.errors++;   break;
+						case RuleStatus.Warning: totals.warnings++; break;
+					}
+					return ruleErrorReport(rule, result);
+			}
+
+			return undefined;
+		})
+		.filter(Boolean);
+
+		if (!options.quiet && reports.length > 0) {
+			const fullTargetPath = getAbsolutePath([process.cwd(), workingDirectory, targetFsPath]) + targetPropertiesPathSegments.replace('.', '#');
+			console.log('\n' + formattedHeader(fullTargetPath) + '\n' + reports.join('\n'));
 		}
-		if (totals.errors > 0) {
-			process.exitCode = 1;
-		}
+	}
+
+	if (!options.quiet && Object.values(totals).some(Boolean)) {
+		console.log('\n' + totalsReport(totals.errors, totals.warnings, totals.skipped) + '\n');
+	}
+	if (totals.errors > 0) {
+		process.exitCode = 1;
 	}
 }
