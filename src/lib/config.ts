4b8354b1d811499e602ef63a59a21eb5ffa2fb0d
cheap-glitch <cheap.glitch@gmail.com>
2021-01-25 21:51:25 +0100

    🌱 feat: Add basic support for extending configs

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,8 +1,11 @@
 import { homedir } from 'os';
+import merge from 'mazeru';
+import { MergingStrategy } from 'mazeru';
 import { JsonValue, JsonObject } from 'type-fest';
 
+import { wrapInArray } from './helpers/utilities';
 import { isJsonObject } from './helpers/json';
-import { readFileContents } from './helpers/fs';
+import { joinPathSegments, readFileContents } from './helpers/fs';
 
 export async function loadConfig(): Promise<JsonObject> {
 	let config: JsonValue;
@@ -15,8 +18,47 @@ export async function loadConfig(): Promise<JsonObject> {
 	}
 
 	if (!isJsonObject(config)) {
-		throw new Error('Invalid config object');
+		throw new Error('invalid config object');
+	}
+
+	return await extendConfig(config);
+}
+
+function extendConfig(config: JsonObject): JsonObject {
+	while (config.extends !== undefined) {
+		for (const extendPath of [...wrapInArray(config.extends)]) {
+			if (typeof extendPath !== 'string') {
+				continue;
+			}
+
+			const baseConfig = resolveExtendPath(extendPath);
+			if (!isJsonObject(baseConfig)) {
+				continue;
+			}
+
+			const mergeResult = merge(baseConfig, config, {
+				arrays:      MergingStrategy.MergeItems,
+				excludeKeys: ['extends'],
+			});
+
+			// TODO: remove when `ignoreBaseKeys` is implemented in `mazeru`
+			mergeResult.root = config.root;
+			config = mergeResult;
+		}
 	}
 
 	return config;
+}
+
+function resolveExtendPath(path: string): JsonValue | undefined {
+	if (path.startsWith('devlint:')) {
+		return require(joinPathSegments([__dirname, '..', '..', '..', 'configs', path.replace('devlint:', '') + '.json']));
+	}
+
+	return undefined;
+
+	// TODO:
+	//   - config module
+	//   - configuration included in plugin
+	//   - absolute & relative path to JSON/JS file
 }
