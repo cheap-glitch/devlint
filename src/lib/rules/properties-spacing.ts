05dfe596a900a3dd9734266675326864283d48db
cheap-glitch <cheap.glitch@gmail.com>
2021-01-28 21:20:54 +0100

    🐛 fix(rules): Fix error location in `properties-spacing`

--- src/lib/rules/properties-spacing.ts
+++ src/lib/rules/properties-spacing.ts
@@ -35,9 +35,9 @@ export function validator({ lines, jsonObjectAst, parameter: position }: RuleCon
 
 	for (const [index, { key, value }] of jsonObjectAst.members.entries()) {
 		if (!checkLineAbove(key.pos.start.line - 1, index)) {
-			return new RuleError('missing empty line above', key.pos, lines);
+			return new RuleError('missing empty line above', key.pos.start, key.pos.start, lines);
 		} else if (!checkLineBelow(value.pos.end.line - 1, index, jsonObjectAst.members.length - 1)) {
-			return new RuleError('missing empty line below', value.pos, lines);
+			return new RuleError('missing empty line below', value.pos.end, value.pos.end, lines);
 		}
 	}
 
