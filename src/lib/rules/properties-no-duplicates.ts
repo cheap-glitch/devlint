2ff327c3aab29ddebe75b5c49e4d4888388ebf50
cheap-glitch <cheap.glitch@gmail.com>
2021-02-02 12:37:47 +0100

    🌱 feat(rules): Apply `properties-no-duplicates` recursively

--- src/lib/rules/properties-no-duplicates.ts
+++ src/lib/rules/properties-no-duplicates.ts
@@ -1,19 +1,27 @@
+import { isJsonObjectAst } from '../helpers/json';
 import { RuleTargetType, RuleContext, RuleResult, RuleError } from '../rules';
 
 export const targetType = RuleTargetType.JsonObject;
 
-export function validator({ lines, jsonObjectAst }: RuleContext): RuleResult {
+export function validator(context: RuleContext): RuleResult {
+	const { lines, jsonObjectAst } = context;
+
 	if (jsonObjectAst.members === undefined) {
 		return true;
 	}
 
-	const properties = jsonObjectAst.members.map(({ key }) => key.value);
-	for (const [index, property] of properties.entries()) {
-		if (properties.indexOf(property) !== index) {
-			return new RuleError(`duplicated property "${property}"`, jsonObjectAst.members[index].key.pos, lines);
+	const keys = jsonObjectAst.members.map(({ key }) => key.value);
+	for (const [index, { key, value }] of jsonObjectAst.members.entries()) {
+		if (keys.indexOf(key.value) !== index) {
+			return new RuleError(`duplicated property "${key.value}"`, key.pos, lines);
 		}
 
-		// TODO: make recursive
+		if (isJsonObjectAst(value)) {
+			const result = validator({ ...context, lines: lines.slice(key.pos.start.line - 1, key.pos.end.line - 1), jsonObjectAst: value });
+			if (result instanceof Error) {
+				return result;
+			}
+		}
 	}
 
 	return true;
