d5e46b6ac9c3810af3e5df429568285fd33e8b14
cheap-glitch <cheap.glitch@gmail.com>
2021-02-05 13:07:48 +0100

    🐛 fix(rules): Rewrite and fix `properties-ordering`

--- src/lib/rules/properties-ordering.ts
+++ src/lib/rules/properties-ordering.ts
@@ -1,37 +1,28 @@
-import { JsonString as JsonStringAst } from 'jsonast';
-
 import { RuleTargetType, RuleContext, RuleResult, RuleError, RuleErrorType } from '../rules';
 
 export const targetType = RuleTargetType.JsonObject;
 
-export function validator({ lines, jsonObjectAst, parameter: ordering }: RuleContext): RuleResult {
-	if (ordering === undefined || typeof ordering !== 'string') {
+export function validator({ lines, jsonObjectAst, parameter: orderingType }: RuleContext): RuleResult {
+	if (orderingType !== 'alphabetical' && orderingType !== 'alphabetical-blocks') {
 		return new RuleError(RuleErrorType.InvalidParameter);
 	}
 	if (jsonObjectAst.members === undefined) {
 		return true;
 	}
 
-	const compare = (new Intl.Collator('en', { numeric: true })).compare;
-
-	let errorChecker;
-	switch (ordering) {
-		case 'alphabetical':
-			errorChecker = (key: JsonStringAst, previousKey: JsonStringAst) => compare(previousKey.value, key.value) > 0;
-			break;
-
-		case 'alphabetical-blocks':
-			errorChecker = (key: JsonStringAst, previousKey: JsonStringAst) => compare(previousKey.value, key.value) > 0 && previousKey.pos.end.line > key.pos.start.line + 1;
-			break;
-
-		default: return new RuleError(RuleErrorType.InvalidParameter);
-	}
+	const compare = new Intl.Collator('en', { numeric: true }).compare;
 
 	for (const [index, { key }] of jsonObjectAst.members.entries()) {
-		const previousKey = jsonObjectAst.members[index - 1]?.key ?? undefined;
-		if (previousKey && errorChecker(key, previousKey)) {
-			return new RuleError(`property "${key.value}" is not in alphabetical order`, key.pos, lines);
+		const { key: previousKey, value: previousValue } = jsonObjectAst.members[index - 1] ?? {};
+		if (previousKey === undefined || previousValue === undefined) {
+			continue;
 		}
+
+		if (compare(previousKey.value, key.value) <= 0 || (orderingType === 'alphabetical-blocks' && previousValue.pos.end.line < key.pos.start.line - 1)) {
+			continue;
+		}
+
+		return new RuleError(`property "${key.value}" is not in alphabetical order`, key.pos, lines);
 	}
 
 	return true;
