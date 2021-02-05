96687293818c4de039442eb6ac74f657d3fbfb9a
cheap-glitch <cheap.glitch@gmail.com>
2021-02-05 11:55:49 +0100

    🌱 feat(rules): Make `ending-period` parameter optional

--- src/lib/rules/ending-period.ts
+++ src/lib/rules/ending-period.ts
@@ -3,12 +3,12 @@ import { RuleTargetType, RuleContext, RuleResult, RuleError, RuleErrorType } fro
 export const targetType = RuleTargetType.JsonString;
 
 export function validator({ lines, jsonString: text, jsonAst, parameter: required }: RuleContext): RuleResult {
-	if (typeof required !== 'boolean') {
+	if (required !== undefined && typeof required !== 'boolean') {
 		return new RuleError(RuleErrorType.InvalidParameter);
 	}
 
-	if (text.endsWith('.') !== required) {
-		return new RuleError(`string ${required ? "doesn't end" : 'ends'} with a period`, jsonAst?.pos, lines);
+	if (text.endsWith('.') !== (required ?? true)) {
+		return new RuleError(`string ${(required ?? true) ? "doesn't end" : 'ends'} with a period`, jsonAst?.pos, lines);
 	}
 
 	return true;
