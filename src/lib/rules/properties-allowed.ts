2d54ca231ad8f83d9e32b4d7b15174370f5fadda
cheap-glitch <cheap.glitch@gmail.com>
2021-02-08 18:41:14 +0100

    🌱 feat(rule): Add `properties-allowed`

---
+++ src/lib/rules/properties-allowed.ts
@@ -0,0 +1,20 @@
+import { RuleTargetType, RuleContext, RuleResult, RuleError, RuleErrorType } from '../rules';
+
+export const targetType = RuleTargetType.JsonObject;
+
+export function validator({ lines, jsonObjectAst, parameter: allowedProperties }: RuleContext): RuleResult {
+	if (!Array.isArray(allowedProperties)) {
+		return new RuleError(RuleErrorType.InvalidParameter);
+	}
+	if (jsonObjectAst.members === undefined) {
+		return true;
+	}
+
+	for (const { key } of jsonObjectAst.members) {
+		if (!allowedProperties.includes(key.value)) {
+			return new RuleError(`property "${key.value}" is not allowed`, key.pos, lines);
+		}
+	}
+
+	return true;
+}
