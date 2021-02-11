0d77c15c842abaf1bff34f67566078420c94903d
cheap-glitch <cheap.glitch@gmail.com>
2021-02-11 19:56:55 +0100

    🌱 feat(rules): Add `elements-forbidden`

---
+++ src/lib/rules/elements-forbidden.ts
@@ -0,0 +1,18 @@
+import { matchJsonValues } from '../helpers/json';
+import { RuleTargetType, RuleContext, RuleResult, RuleError, RuleErrorType } from '../rules';
+
+export const targetType = RuleTargetType.JsonArray;
+
+export function validator({ lines, jsonArray, jsonArrayAst, parameter: forbiddenElements }: RuleContext): RuleResult {
+	if (!Array.isArray(forbiddenElements)) {
+		return new RuleError(RuleErrorType.InvalidParameter);
+	}
+
+	for (const [index, element] of jsonArray.entries()) {
+		if (forbiddenElements.some(forbiddenElement => matchJsonValues(forbiddenElement, element))) {
+			return new RuleError(`element "${element}" is forbidden`, jsonArrayAst.elements?.[index]?.pos, lines);
+		}
+	}
+
+	return true;
+}
