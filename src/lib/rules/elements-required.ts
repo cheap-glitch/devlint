a851bf3eb71858dccefdb3ab0fa99a01ceb2bd28
cheap-glitch <cheap.glitch@gmail.com>
2021-02-11 19:28:13 +0100

    🌱 feat(rules): Add `elements-required`

---
+++ src/lib/rules/elements-required.ts
@@ -0,0 +1,20 @@
+import { matchJsonValues } from '../helpers/json';
+import { RuleTargetType, RuleContext, RuleResult, RuleError, RuleErrorType } from '../rules';
+
+export const targetType = RuleTargetType.JsonArray;
+
+export function validator({ lines, jsonArray, jsonArrayAst, parameter: requiredElements }: RuleContext): RuleResult {
+	if (!Array.isArray(requiredElements)) {
+		return new RuleError(RuleErrorType.InvalidParameter);
+	}
+
+	for (const requiredElement of requiredElements) {
+		if (jsonArray.some(element => matchJsonValues(requiredElement, element))) {
+			continue;
+		}
+
+		return new RuleError(`required element "${requiredElement}" is missing`, jsonArrayAst.pos, lines);
+	}
+
+	return true;
+}
