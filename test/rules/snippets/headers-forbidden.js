46f29d58dc0e21dbe8a7161dc6b0d9c5fcebd930
cheap-glitch <cheap.glitch@gmail.com>
2021-03-14 18:16:36 +0100

    🚩 test(rules): Add tests for `headers-forbidden`

--- test/rules/snippets/headers-forbidden.js
+++ test/rules/snippets/headers-forbidden.js
@@ -1,8 +1,97 @@
+const outdent = require('outdent');
+
 module.exports = {
 	passing: [
+		[outdent`
+			# Header
+
+			Paragraph
+			`,
+			['Foo'],
+		],
+
+		[outdent`
+			# Header
+
+			Paragraph
+
+			## Foo
+
+			Paragraph
+			`,
+			[],
+		],
+
+		[outdent`
+			# Header
+
+			Paragraph
+
+			## Foo
+
+			Paragraph
+			`,
+			['Bar'],
+		],
+
+		[outdent`
+			# Header
+
+			Paragraph
+
+			## Foo
+
+			Paragraph
+			`,
+			['# Foo', '### Foo'],
+		],
 	],
 
 	failing: [
-		[['## Lorem ipsum', ['Lorem ipsum']], 'header "Lorem ipsum" is forbidden', { line: 1, column: 1, char: 0 }, { line: 1, column: 14, char: 13 }],
+		[['', true],                      2],
+		[['', 'Foo'],                     2],
+		[['', '## Foo'],                  2],
+		[['', { text: 'Foo', level: 2 }], 2],
+
+		[[outdent`
+			# Header
+
+			Paragraph
+
+			## Foo
+
+			Paragraph
+			`,
+			['Foo'],
+		], 'header "Foo" is forbidden', { line: 5, column: 1, char: 21 }, { line: 5, column: 6, char: 26 }],
+
+		[[outdent`
+			# Header
+
+			Paragraph
+
+			## Foo
+
+			Paragraph
+			`,
+			['## Foo'],
+		], 'header "Foo" is forbidden', { line: 5, column: 1, char: 21 }, { line: 5, column: 6, char: 26 }],
+
+		[[outdent`
+			# Foo
+
+			Paragraph
+
+			### Foo
+
+			 - List item
+			 - List item
+
+			## Foo
+
+			Paragraph
+			`,
+			['## Foo'],
+		], 'header "Foo" is forbidden', { line: 10, column: 1, char: 54 }, { line: 10, column: 6, char: 59 }],
 	],
 };
