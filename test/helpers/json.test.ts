10abcc29b244008e3bc67144d1620dc86ae6372c
cheap-glitch <cheap.glitch@gmail.com>
2022-03-04 11:09:54 +0100

    🚩 test: Add tests for JSON helpers

---
+++ test/helpers/json.test.ts
@@ -0,0 +1,36 @@
+import { matchJsonValues } from '../../src/lib/helpers/json';
+
+describe('matchJsonValues', () => {
+
+	test('primitives', () => { // {{{
+
+		expect(matchJsonValues()).toBe(true);
+		expect(matchJsonValues(null, null)).toBe(true);
+		expect(matchJsonValues(false, false)).toBe(true);
+		expect(matchJsonValues(1, 1)).toBe(true);
+		expect(matchJsonValues('foo', 'foo')).toBe(true);
+
+		expect(matchJsonValues(1, false)).toBe(false);
+		expect(matchJsonValues(1, '1')).toBe(false);
+		expect(matchJsonValues('foo', 'bar')).toBe(false);
+
+	}); // }}}
+
+	test('arrays', () => { // {{{
+
+		expect(matchJsonValues([], [])).toBe(true);
+		expect(matchJsonValues([true], [true])).toBe(true);
+
+		expect(matchJsonValues([true], [false])).toBe(false);
+
+	}); // }}}
+
+	test('objects', () => { // {{{
+
+		expect(matchJsonValues({}, {})).toBe(true);
+
+		expect(matchJsonValues({}, { foo: 1 })).toBe(true);
+
+	}); // }}}
+
+});
